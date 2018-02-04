bfc41c8d5380e45621eceac39ef255df7959b60c
smartmimo <el.attaoui.mohamed@gmail.com>
2018-02-04 05:06:06 +0000

    slm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('Life', {type: 'THINKING ABOUT'});
 });
 
 client.on('message', msg => {
